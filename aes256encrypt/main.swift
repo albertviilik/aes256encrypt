50f7f6de815147b89a9614cd08faba837c3bd36f
Albert Viilik <42554671+albertviilik@users.noreply.github.com>
2021-01-04 22:42:13 +0000

    Version 1.0.0

--- aes256encrypt/main.swift
+++ aes256encrypt/main.swift
@@ -6,6 +6,57 @@
 //
 
 import Foundation
+import CryptoKit
 
-print("Hello, World!")
+let usage = "./aes256encrypt [enc/dec] [message] [key]"
 
+if CommandLine.arguments.count < 3 || (CommandLine.arguments[1] != "enc" && CommandLine.arguments[1] != "dec") {
+    print(usage)
+    exit(1)
+}
+
+var messageBytes: Data
+var keyBytes: Data
+
+guard let keyBytes = CommandLine.arguments[3].data(using: .utf8), let messageBytes = CommandLine.arguments[2].data(using: .utf8) else {
+    print("Error: Failed to retrieve message or key bytes!")
+    exit(1)
+}
+
+let keyHash = SHA256.hash(data: keyBytes)
+
+let symmetricKey = SymmetricKey(data: keyHash)
+
+if (CommandLine.arguments[1] == "enc") {
+    
+    guard let sealed = try? AES.GCM.seal(messageBytes, using: symmetricKey) else {
+        print("Error: Failed to encrypt!")
+        exit(1)
+    }
+    
+    guard let data = sealed.combined else {
+        print("Error: Failed to retrieve combined data!")
+        exit(1)
+    }
+    
+    print(data.base64EncodedString())
+    
+} else {
+    
+    guard let combinedData = Data(base64Encoded: CommandLine.arguments[2]) else {
+        print("Error0: Failed to retrieve combined data!")
+        exit(1)
+    }
+    
+    guard let sealed = try? AES.GCM.SealedBox(combined: combinedData) else {
+        print("Error1: Failed to construct a sealed box from the data!")
+        exit(1)
+    }
+    
+    guard let unsealed = try? AES.GCM.open(sealed, using: symmetricKey) else {
+        print("Error: Wrong key!")
+        exit(1)
+    }
+    
+    print(String(decoding: unsealed, as: UTF8.self))
+}
